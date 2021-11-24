4d587c21e8d719deae5b95d958c6855c55e4947d
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-11-24 17:42:12 +0530

    Consuming Store

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 import { useActions } from '../hooks/useActions'
 
 const RepositoriesList: React.FC = () => {
   const { searchRepositories } = useActions()
   const [term, setTerm] = useState('')
+
+  const { data, error, loading } = useTypedSelector(state => state.repositories)
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -21,6 +24,10 @@ const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
+
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map(name => <div key={name}>{name}</div>)}
     </div>
   )
 }
